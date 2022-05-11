9b06b6614a19c9b67b343d010c12b1bdc5c80860
IUTB <noreply@univ-tlse2.fr>
2022-05-11 17:36:42 +0200

    javadoc

--- WorkSpaceJavaFX/Daily_Bank_V0/DAILY_BANK_FX_V0/src/application/DailyBankState.java
+++ WorkSpaceJavaFX/Daily_Bank_V0/DAILY_BANK_FX_V0/src/application/DailyBankState.java
@@ -9,30 +9,52 @@ public class DailyBankState {
 	private AgenceBancaire agAct;
 	private boolean isChefDAgence;
 
+	
+	/**
+	 * @return Retourne l'employé actif
+	 */
 	public Employe getEmpAct() {
 		return this.empAct;
 	}
 
+	/**
+	 * @param Remplace l'employé actif par le paramètre employeActif
+	 */
 	public void setEmpAct(Employe employeActif) {
 		this.empAct = employeActif;
 	}
 
+	/**
+	 * @return Retourne l'agence active
+	 */
 	public AgenceBancaire getAgAct() {
 		return this.agAct;
 	}
 
+	/**
+	 * @param Remplace l'agence active par le paramètre agenceActive
+	 */
 	public void setAgAct(AgenceBancaire agenceActive) {
 		this.agAct = agenceActive;
 	}
 
+	/**
+	 * @return Indique si l'utilisateur est chef d'agence
+	 */
 	public boolean isChefDAgence() {
 		return this.isChefDAgence;
 	}
 
+	/**
+	 * @param Change l'état d'un utilisateur en chef d'agence ou non : isChefDAgence
+	 */
 	public void setChefDAgence(boolean isChefDAgence) {
 		this.isChefDAgence = isChefDAgence;
 	}
 
+	/**
+	 * @param Vérifie si l'utilsateur est chef d'agence grâce auxd droits droitAccess. Si non : il n'est pas mis chef d'agence. Si oui, il est mis chef d'agence.
+	 */
 	public void setChefDAgence(String droitsAccess) {
 		this.isChefDAgence = false;
 
