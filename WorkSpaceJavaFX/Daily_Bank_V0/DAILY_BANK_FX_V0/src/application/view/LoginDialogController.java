d7223e0be07f408ee42db02be7452da83adb442b
Mazlai | マイ <57178136+Mazlai@users.noreply.github.com>
2022-05-25 14:36:27 +0200

    Javadoc - LoginDialogController

--- WorkSpaceJavaFX/Daily_Bank_V0/DAILY_BANK_FX_V0/src/application/view/LoginDialogController.java
+++ WorkSpaceJavaFX/Daily_Bank_V0/DAILY_BANK_FX_V0/src/application/view/LoginDialogController.java
@@ -14,6 +14,11 @@ import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import model.data.Employe;
 
+/**
+ * La classe "LoginDialogController" permet de traiter l'ensemble des actions de l'utilisateur concernant la définition d'un membre d'une agence bancaire lors de son authentification. 
+ * Cette classe traite ainsi les données renseignées par l'utilisateur, à la fois sur la "vue", correspondant à la partie graphique de l'interface mais également dans le "modèle", signifiant l'univers dans lequel s'inscrit l'application.
+ */
+
 public class LoginDialogController implements Initializable {
 
 	// Etat application
@@ -33,10 +38,16 @@ public class LoginDialogController implements Initializable {
 		this.configure();
 	}
 
+	/**
+	 * Permet de mettre en place la configuration de la fenêtre (ainsi que les données chargées). 
+	 */
 	private void configure() {
 		this.primaryStage.setOnCloseRequest(e -> this.closeWindow(e));
 	}
 
+	/**
+	 * Affiche simplement la fenêtre en question, tout en attendant une quelconque réponse de l'utilisateur.
+	 */
 	public void displayDialog() {
 		this.primaryStage.showAndWait();
 	}
@@ -60,12 +71,19 @@ public class LoginDialogController implements Initializable {
 	public void initialize(URL location, ResourceBundle resources) {
 	}
 
+	/**
+	 * Permet l'annulation de modification sur la page et donc, de retourner sur la page précédente, en fermant la fenêtre actuelle.
+	 */
 	@FXML
 	private void doCancel() {
 		this.dbs.setEmpAct(null);
 		this.primaryStage.close();
 	}
 
+	/**
+	 * Permet de traiter les différentes conditions de renseignement dans les champs de texte pour un utilisateur. 
+	 * Renseigne une erreur si l'une des conditions n'est pas respectée, ou rien et ferme la fenêtre dès lors que les informations sont correctes.
+	 */
 	@FXML
 	private void doOK() {
 		String login = this.txtLogin.getText().trim();
@@ -84,6 +102,10 @@ public class LoginDialogController implements Initializable {
 		}
 	}
 
+	/**
+	 * Permet d'afficher un message d'erreur lorsque l'utilisateur n'a pas respecté l'une des conditions nécessaires à son authentification.
+	 * @param texte IN : le texte affichant le message d'erreur
+	 */
 	private void afficheErreur(String texte) {
 		this.lblMessage.setText(texte);
 		this.lblMessage.setStyle("-fx-text-fill:red; -fx-font-weight: bold;");
