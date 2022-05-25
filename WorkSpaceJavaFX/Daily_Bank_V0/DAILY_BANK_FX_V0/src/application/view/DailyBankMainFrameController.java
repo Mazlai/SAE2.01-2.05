1d7e4e04edb96706d00bd2704b80a0b9816848f4
Mazlai | マイ <57178136+Mazlai@users.noreply.github.com>
2022-05-25 14:44:49 +0200

    Javadoc - DailyBankMainFrameController

--- WorkSpaceJavaFX/Daily_Bank_V0/DAILY_BANK_FX_V0/src/application/view/DailyBankMainFrameController.java
+++ WorkSpaceJavaFX/Daily_Bank_V0/DAILY_BANK_FX_V0/src/application/view/DailyBankMainFrameController.java
@@ -17,6 +17,11 @@ import javafx.stage.WindowEvent;
 import model.data.AgenceBancaire;
 import model.data.Employe;
 
+/**
+ * La classe "DailyBankMainFrameController" correspond à la fenêtre principale de l'application et propose à un employé de l'agence, plusieurs actions qu'il puisse réaliser (gérer les clients, etc.)
+ * Cette classe traite ainsi les données utilisées par l'utilisateur, à la fois sur la "vue", correspondant à la partie graphique de l'interface mais également dans le "modèle", signifiant l'univers dans lequel s'inscrit l'application.
+ */
+
 public class DailyBankMainFrameController implements Initializable {
 
 	// Etat application
@@ -37,10 +42,16 @@ public class DailyBankMainFrameController implements Initializable {
 		this.validateComponentState();
 	}
 
+	/**
+	 * Affiche simplement la fenêtre en question, sans nécessairement attendre une quelconque réponse de l'utilisateur.
+	 */
 	public void displayDialog() {
 		this.primaryStage.show();
 	}
 
+	/**
+	 * Permet de mettre en place la configuration de la fenêtre ainsi que les données chargées. 
+	 */
 	private void configure() {
 		this.primaryStage.setOnCloseRequest(e -> this.closeWindow(e));
 		this.btnConn.managedProperty().bind(this.btnConn.visibleProperty());
@@ -82,6 +93,9 @@ public class DailyBankMainFrameController implements Initializable {
 	public void initialize(URL location, ResourceBundle resources) {
 	}
 
+	/**
+	 * Indique si l'utilisateur souhaiter fermer l'application "DailyBankApp", dépendant du futur choix de l'utilisateur.
+	 */
 	@FXML
 	private void doQuit() {
 
@@ -93,12 +107,18 @@ public class DailyBankMainFrameController implements Initializable {
 		}
 	}
 
+	/**
+	 * Permet d'indiquer les mentions ("credits") de l'application et de sa conception.
+	 */
 	@FXML
 	private void doActionAide() {
 		String contenu = "DailyBank v1.01\nSAE 2.01 Développement\nIUT-Blagnac";
 		AlertUtilities.showAlert(this.primaryStage, "Aide", null, contenu, AlertType.INFORMATION);
 	}
 
+	/**
+	 * Permet à l'utilisateur de se connecter en renseignant des textes valides, et selon si l'employé existe, et figurant dans l'agence bancaire. 
+	 */
 	@FXML
 	private void doLogin() {
 
@@ -106,12 +126,18 @@ public class DailyBankMainFrameController implements Initializable {
 		this.validateComponentState();
 	}
 
+	/**
+	 * Permet à l'utilisateur de se déconnecter de son compte, tout en dépendant de l'existence même de l'employé.
+	 */
 	@FXML
 	private void doDisconnect() {
 		this.dbmf.disconnect();
 		this.validateComponentState();
 	}
 
+	/**
+	 * Permet de définir l'état des différents boutons et labels dédiés à une interaction potentielle de l'utilisateur.
+	 */
 	private void validateComponentState() {
 		Employe e = this.dbs.getEmpAct();
 		AgenceBancaire a = this.dbs.getAgAct();
@@ -145,17 +171,26 @@ public class DailyBankMainFrameController implements Initializable {
 		}
 	}
 
+	/**
+	 * Permet de pouvoir gérer l'ensemble des clients.
+	 */
 	@FXML
 	private void doClientOption() {
 		this.dbmf.gestionClients();
 	}
 
+	/**
+	 * Permet de pouvoir gérer l'ensemble des employés au sein d'une agence bancaire.
+	 */
 	@FXML
 	private void doEmployeOption() {
 		AlertUtilities.showAlert(this.primaryStage, "Gestion des Employé", "En cours de développement",
 				"Livraison prévue\nEn juin 2022", AlertType.INFORMATION);
 	}
 
+	/**
+	 * Permet de se déconnecter de la fenêtre principale.
+	 */
 	private void actionQuitterBD() {
 		this.dbmf.disconnect();
 	}
