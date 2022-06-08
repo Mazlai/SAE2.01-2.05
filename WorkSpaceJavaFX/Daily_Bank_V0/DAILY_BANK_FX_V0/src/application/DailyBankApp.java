8cb2119a72d41de67222defd6ff64e186e1346fb
VictorThompsonKeyl <53653199+VictorThompsonKeyl@users.noreply.github.com>
2022-06-08 19:21:53 +0200

    Javadoc

--- WorkSpaceJavaFX/Daily_Bank_V0/DAILY_BANK_FX_V0/src/application/DailyBankApp.java
+++ WorkSpaceJavaFX/Daily_Bank_V0/DAILY_BANK_FX_V0/src/application/DailyBankApp.java
@@ -2,6 +2,11 @@ package application;
 
 import application.control.DailyBankMainFrame;
 
+/**
+ * Classe principale dont le main se charge de l'exécution de l'application "DailyBankApp".
+ * Cette dernière fait appel la classe "DailyBankMainFrame" correspondant à la fenêtre principale lors de l'exécution de l'application.
+ * L'ensemble des actions en relation avec la fenêtre principale seront réalisées dans le controlleur "DailyBankMainFrameController".
+ */
 public class DailyBankApp  {
 
 	public static void main(String[] args) {
